ff1683baffd60299a94042f8211a1c830668f5d3
Kuba Sejdak <kuba.sejdak@gmail.com>
2019-12-28 13:11:43 +0100

    Add tools.

---
+++ tools/check-clang-format.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# $1... - paths to look for source files
+
+echo "Running clang-format..."
+
+for SRC_PATH in "$@"; do
+    ALL_SOURCES=$(find "${SRC_PATH}" -iname "*.h" -o -iname "*.hpp" -o -iname "*.c" -o -iname "*.cpp")
+    TO_CHECK_SOURCES=$(echo "${TO_CHECK_SOURCES}" | sed '/FreeRTOSConfig.h/d')
+
+    echo "${TO_CHECK_SOURCES}" | xargs clang-format -style=file -fallback-style=none -i
+done
+
+echo "Done."
+exit 0
