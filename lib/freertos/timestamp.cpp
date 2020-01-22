235cb933b7825d4a23b928d74348628ea02242ef
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-01-22 07:56:38 +0100

    Add stubs for freertos implementation.

---
+++ lib/freertos/timestamp.cpp
@@ -0,0 +1,62 @@
+/////////////////////////////////////////////////////////////////////////////////////
+///
+/// @file
+/// @author Kuba Sejdak
+/// @copyright BSD 2-Clause License
+///
+/// Copyright (c) 2020-2020, Kuba Sejdak <kuba.sejdak@gmail.com>
+/// All rights reserved.
+///
+/// Redistribution and use in source and binary forms, with or without
+/// modification, are permitted provided that the following conditions are met:
+///
+/// 1. Redistributions of source code must retain the above copyright notice, this
+///    list of conditions and the following disclaimer.
+///
+/// 2. Redistributions in binary form must reproduce the above copyright notice,
+///    this list of conditions and the following disclaimer in the documentation
+///    and/or other materials provided with the distribution.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+/// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+/// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+/// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+/// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+/// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+/// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+/// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+/// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///
+/////////////////////////////////////////////////////////////////////////////////////
+
+#include "osal/timestamp.h"
+
+//#include <chrono>
+
+//std::chrono::steady_clock::time_point initTime;
+
+//template <typename Unit>
+//static std::uint64_t timeSinceStart()
+//{
+//    auto now = std::chrono::steady_clock::now();
+//    return std::chrono::duration_cast<Unit>(initTime - now).count();
+//}
+
+uint64_t osalTimestampGetMs()
+{
+    return 0;
+//    return timeSinceStart<std::chrono::milliseconds>();
+}
+
+uint64_t osalTimestampGetUs()
+{
+    return 0;
+//   return timeSinceStart<std::chrono::microseconds>();
+}
+
+uint64_t osalTimestampGetNs()
+{
+    return 0;
+//    return timeSinceStart<std::chrono::nanoseconds>();
+}
